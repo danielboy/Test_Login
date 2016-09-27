7a6b841c4645ed2002498d74d871d581647f857d
Daniel Casillas Jimenez <danielcasi@hotmail.com>
2016-09-26 19:37:54 -0600

    se acomodo interfaz resultado se la comia la pantalla

--- app/pages/login/login.ts
+++ app/pages/login/login.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import {NavController} from 'ionic-angular';
+import {NavController, AlertController} from 'ionic-angular';
 import {AuthService} from '../../services/authservice';
 import {UserPage} from '../user/user';
 import {SignupPage} from '../signup/signup';
@@ -12,7 +12,8 @@ export class LoginPage {
     private usercreds: any;
     private service: any;
     private nav: any;
-    constructor(private authservice: AuthService, private navcontroller: NavController) {
+    private dusers: any;
+    constructor(private authservice: AuthService, private navcontroller: NavController, private alertCtrl: AlertController) {
         this.usercreds = {
             name: '',
             password: ''
@@ -21,10 +22,23 @@ export class LoginPage {
         this.nav = navcontroller;
     }
     login(user) {
+        this.dusers = user;
+                 if(user == null){
+            let alert = this.alertCtrl.create({
+                title: 'Atención',
+                subTitle: 'Introduce tu Nombre y Contraseña',
+                buttons: ['Ok']
+                });
+            alert.present();
+
+            return;
+        }
         this.service.authenticate(user).then(data => {
             if(data) {
                 this.nav.setRoot(UserPage);
             }
+
+
     });
 }
     signup() {
